521e8488d66f65dff3d0ac19bd839d9c4458a958
jkruppa <jochen.kruppa@gmail.com>
2016-08-04 15:04:25 +0200

    First function testing

---
+++ R/basePyramid.R
@@ -0,0 +1,12 @@
+##' This is a test function
+##'
+##' This is a test function
+##' @title This is a test function
+##' @param foo 
+##' @param bar 
+##' @return NULL
+##' @author Jochen Kruppa
+##' @export
+baseFunction <- function(foo, bar){
+    print(foo)
+}
