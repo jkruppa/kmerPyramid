a2a22cda395b5b5c849d4ec59455ed1ff200d6c5
jkruppa <jochen.kruppa@gmail.com>
2016-08-10 12:02:41 +0200

    Add first data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' k-mer distribution of virus DNA sequences
+#'
+#' A dataset containing containing the 1, 2, 3, 4, and 5-mer
+#' distribution of the nucleic acid bases of 5785 viruses available as
+#' embl files from NCBI.
+#'
+#' @format A data frame with 5785 rows and 1365 variables:
+#' \describe{
+#'   \item{id}{Name of the virus}
+#'   \item{A}{Percentage of nucleic acid base A in the 1-mers}
+#'   \item{TTTTT}{Percentage of nucleic acid mer TTTTT in the 5-mers}
+#' }
+#' @source \url{http://www.ncbi.nlm.nih.gov/pmc/articles/PMC99098/}
+"virusKmerDistribution"
