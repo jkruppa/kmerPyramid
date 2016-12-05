5352de5a1e710a443ff5e00015333c3715f028a2
jkruppa <jochen.kruppa@gmail.com>
2016-12-05 14:34:23 +0100

    some data information

--- R/data.R
+++ R/data.R
@@ -4,9 +4,11 @@
 #' distribution of the nucleic acid bases of 5785 viruses available as
 #' embl files from NCBI.
 #'
-#' @format A data frame with 5785 rows and 1365 variables:
+#' @format A data frame with 5000+ rows and 1367 variables:
 #' \describe{
-#'   \item{id}{Name of the virus}
+#'   \item{id}{Name of the virus (NCBI)}
+#'   \item{type}{Type of the virus}
+#'   \item{species}{Species of the virus}
 #'   \item{A}{Percentage of nucleic acid base A in the 1-mers}
 #'   \item{...}{...}
 #'   \item{TTTTT}{Percentage of nucleic acid mer TTTTT in the 5-mers}
@@ -18,12 +20,14 @@
 #' k-mer distribution of bacteria DNA sequences
 #'
 #' A dataset containing containing the 1, 2, 3, 4, and 5-mer
-#' distribution of the nucleic acid bases of 5785 viruses available as
+#' distribution of the nucleic acid bases of bacteria available as
 #' embl files from NCBI.
 #'
-#' @format A data frame with 5785 rows and 1365 variables:
+#' @format A data frame with 2000+ rows and 1365 variables:
 #' \describe{
-#'   \item{id}{Name of the virus}
+#'   \item{id}{Name of the bacteria (NCBI)}
+#'   \item{type}{Type of the bacteria}
+#'   \item{species}{Species of the bacteria}
 #'   \item{A}{Percentage of nucleic acid base A in the 1-mers}
 #'   \item{...}{...}
 #'   \item{TTTTT}{Percentage of nucleic acid mer TTTTT in the 5-mers}
